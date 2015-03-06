d89b8fe7bdc69d1716a68349b19ed7cb08f409b2
Nathan Boley <npboley@gmail.com>
2015-03-05 18:23:53 -0800

    Actually implemented PC algorithm...

--- test_my_pc.py
+++ test_my_pc.py
@@ -1,5 +1,7 @@
 import os, sys
 
+import math
+
 from collections import defaultdict
 
 from itertools import combinations
@@ -16,10 +18,10 @@ from numpy.linalg import pinv
 import numpy as np
 from scipy import stats, linalg
 
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 from sklearn.feature_selection import f_regression
 from scipy.linalg import lstsq
-from scipy.stats import f
+from scipy.stats import f, norm, pearsonr
 
 #print numpy.random.seed()
 try: 
@@ -28,6 +30,7 @@ except:
     seed = random.randrange(100)
     print "SEED:", seed
     numpy.random.seed(seed)
+# 82 is a good seed 
 
 VERBOSE = False 
 REVERSE_CAUSALITY = False
@@ -38,20 +41,21 @@ def partial_corr(C):
         numpy.outer(numpy.diag(inv_cov), numpy.diag(inv_cov)))
     return inv_cov/normalization_mat
 
-def add_children(G, parent, num):
-    child_level = G.node[parent]['level'] + 1
-    children_ids = []
-    for i in xrange(num):
-        id = "%i_%i_%i" % (child_level, G.node[parent]['order'], i)
-        G.add_node(id, level=child_level, order=i, parent_order=G.node[parent]['order'])
-        if not REVERSE_CAUSALITY:
-            G.add_edge(parent, id)
-        else:
-            G.add_edge(id, parent)
-        yield id
-    return
 
 def build_causal_graph(n_children, max_height):
+    def add_children(G, parent, num):
+        child_level = G.node[parent]['level'] + 1
+        children_ids = []
+        for i in xrange(num):
+            id = "%i_%i_%i" % (child_level, G.node[parent]['order'], i)
+            G.add_node(id, level=child_level, order=i, parent_order=G.node[parent]['order'])
+            if not REVERSE_CAUSALITY:
+                G.add_edge(parent, id)
+            else:
+                G.add_edge(id, parent)
+            yield id
+        return
+    
     G = nx.DiGraph()
     G.add_node("0", level=0, order=0)
     pending_nodes = ["0",]
@@ -91,7 +95,7 @@ def simulate_data_from_causal_graph(G, n_timepoints, corr=0.9):
     return ( sorted(expression_values), 
              numpy.vstack(y for x, y in sorted(expression_values.items())))
 
-def estimate_covariates(sample1, sample2, i):
+def estimate_covariates(sample1, sample2, resp_index, alpha=0.50):
     def cv_gen():
         n_tps = sample1.shape[1]
         yield range(n_tps), range(n_tps, 2*n_tps)
@@ -100,39 +104,85 @@ def estimate_covariates(sample1, sample2, i):
     
     merged_samples = numpy.hstack((sample1, sample2))
     # normalize to sum 1
-    merged_samples = (merged_samples.T/(merged_samples.sum(1)+1)).T
-    #print merged_samples
-    #assert False
-    mask = numpy.ones(merged_samples.shape[0], dtype=bool)
-    mask[i] = False
-    
-    response = merged_samples[i,:]
+    merged_samples = ((merged_samples.T)/(merged_samples.sum(1))).T
+
+    pred_mask = numpy.ones(merged_samples.shape[0], dtype=bool)
+    pred_mask[resp_index] = False
+
+    # filter marginally uncorrelated covariates
+    response = merged_samples[resp_index,:]    
+    best_i, min_pval = None, 10
+    for i, row in enumerate(merged_samples):
+        if i == resp_index: continue
+        corr_coef, p_value = pearsonr(row, response)
+        if p_value > alpha: pred_mask[i] = False
+        if p_value < min_pval: best_i = i
+    pred_mask[best_i] = True
     
     clf = linear_model.LassoCV(
        fit_intercept=True, cv=cv_gen(), verbose=0, max_iter=100000)
-    clf.fit(merged_samples[mask,:].T, response.T)
+    clf.fit(merged_samples[pred_mask,:].T, response.T)
     
     alpha = clf.alpha_
-    coefs = clf.coef_.tolist()
-    coefs.insert(i, 0)
     
+    clf = linear_model.Lasso(
+       alpha=alpha, max_iter=100000)
+    clf.fit(merged_samples[pred_mask,:].T, response.T)
+
+    coefs = clf.coef_.tolist()
+    for i in sorted(int(x) for x in (1-pred_mask).nonzero()[0]):
+        coefs.insert(i, 0)
     return alpha, coefs
 
 
-def estimate_skeleton_from_samples(sample1, sample2, labels, thresh_ratio=10):
+def estimate_skeleton_from_samples(sample1, sample2, labels, thresh_ratio=1000):
     G = nx.DiGraph()
     for i in xrange(sample1.shape[0]):
         G.add_node(i, label=labels[i])
         alpha, regression_coefs = estimate_covariates(sample1, sample2, i)
-        max_coef = max(regression_coefs)
-        print i, alpha, regression_coefs
+        max_coef = max(numpy.abs(regression_coefs))
         for j, val in enumerate(regression_coefs):
             if abs(val) > 1e-6 and abs(val) >= max_coef/thresh_ratio:
                 G.add_edge(i, j, weight=val)
-    # remove non bi-directed edges
+    
+    # remove non bi-directed edges and set the edge weights
     for a, b in list(G.edges()):
-        if not G.has_edge(b, a): G.remove_edge(a, b)
+        if not G.has_edge(b, a): 
+            G.remove_edge(a, b)
+        else:
+            G[a][b]['weight'] = min(G[a][b]['weight'], G[b][a]['weight'])
+    
     return G.to_undirected()
+
+def estimate_marginal_correlations(merged_samples, resp_index, 
+                                   min_num_neighbors=1, 
+                                   max_num_neighbors=12,
+                                   alpha=0.05):
+    sig_samples = []
+    response = merged_samples[resp_index,:]
+    for i, row in enumerate(merged_samples):
+        if i == resp_index: continue
+        corr_coef, p_value = pearsonr(row, response)
+        if p_value < alpha or len(sig_samples) < min_num_neighbors: 
+            sig_samples.append((p_value, corr_coef, i))
+    sig_samples.sort(key=lambda x: (x[0], -x[1]))
+    return sig_samples[:max_num_neighbors]
+
+def estimate_initial_skeleton(sample1, sample2, labels, thresh_ratio=1000):
+    # merge and mnormalize the data
+    merged_samples = numpy.hstack((sample1, sample2))
+    merged_samples = ((merged_samples.T)/(merged_samples.sum(1))).T
+    
+    # initialize a graph storing the covariance structure
+    G = nx.Graph()
+    for i in xrange(sample1.shape[0]):
+        if not G.has_node(i): G.add_node(i, label=labels[i])
+        for p, corr, j in estimate_marginal_correlations(merged_samples, i):
+            if not G.has_node(j): G.add_node(j, label=labels[j])
+            G.add_edge(i, j, corr=corr)
+    
+    return G
+
 
 def find_unoriented_edges(G):
     unoriented_edges = set()
@@ -142,7 +192,7 @@ def find_unoriented_edges(G):
             unoriented_edges.add((start, stop))
     return sorted(unoriented_edges)
 
-def iter_v_structures(G):
+def iter_unoriented_v_structures(G):
     for node, node_data in G.nodes(data=True):
         neighbors = [neighbor for neighbor in nx.all_neighbors(G, node)
                      if G.has_edge(neighbor, node) and G.has_edge(node, neighbor)]
@@ -152,7 +202,20 @@ def iter_v_structures(G):
                     continue
                 if G.has_edge(n1, n2) or G.has_edge(n2, n1): 
                     continue
-                yield n1, n2, node
+                yield n1, node, n2
+    return
+
+def iter_colliders(G):
+    for node, node_data in G.nodes(data=True):
+        predecessors = [predecessor for predecessor in G.predecessors(node)
+                        if not G.has_edge(node, predecessor) 
+                        and G.has_edge(predecessor, node)]
+        for n1 in predecessors:
+            assert G.has_edge(n1, node)
+            assert not G.has_edge(node, n1)
+            for n2 in predecessors:
+                if n1 >= n2: continue
+                yield n1, node, n2
     return
 
 def are_cond_indep(a, b, c, data):
@@ -178,7 +241,7 @@ def are_cond_indep(a, b, c, data):
         return False
 
 def orient_single_v_structure(G, data):
-    for a, b, c in iter_v_structures(G):
+    for a, c, b in iter_unoriented_v_structures(G):
         if not are_cond_indep(a, b, c, data):
             # remove the edges point from c to a and b
             try: G.remove_edge(c, a)
@@ -205,8 +268,14 @@ def orient_single_v_structure(G, data):
             return True
     return False
 
-def orient_v_structures(G, data):
-    while orient_single_v_structure(G, data): pass
+def orient_v_structures(G, ci_sets):
+    for a, c, b in iter_unoriented_v_structures(G):
+        if c not in ci_sets[(a,b)]:
+            # remove the edges point from c to a and b
+            try: G.remove_edge(c, a)
+            except: pass
+            try: G.remove_edge(c, b)
+            except: pass
     return
 
 def apply_rule_1(b, c, G):
@@ -293,13 +362,94 @@ def apply_IC_rules(G):
             return True
     return False
 
-def estimate_pdag(sample1, sample2, labels):
+
+def break_cycles(G):
+    G = G.copy()
+    cycles = nx.cycle_basis(G)
+    while len(cycles) > 0:
+        # break cycles
+        edge_cnts = defaultdict(int)
+        edge_weights = {}
+        for cycle in nx.cycle_basis(G):
+            for a,b in zip(cycle[:-1], cycle[1:]) + [(cycle[-1],cycle[0]),]:
+                a, b = min(a,b), max(a,b)
+                edge_cnts[(a,b)] += 1
+                edge_weights[(a,b)] = G[a][b]['weight']
+        edge = sorted(
+            edge_cnts, key=lambda x: (-edge_cnts[x], abs(edge_weights[x])))[0]
+        #print edge, edge_cnts[edge], abs(edge_weights[edge])
+        #print "Removing edge:", edge
+        G.remove_edge(*edge)
+        cycles = nx.cycle_basis(G)
+    return G
+
+def estimate_pdag_OLD(sample1, sample2, labels):
     skeleton = estimate_skeleton_from_samples(sample1, sample2, labels)
+    skeleton = break_cycles(skeleton)
     est_G = skeleton.to_directed()
     orient_v_structures(est_G, numpy.hstack((sample1, sample2)))
     applied_rule = True
     while apply_IC_rules(est_G): pass
+    #assert False
     return est_G
+
+def apply_pc_iteration(G, sample1, sample2, order, alpha=0.05):
+    # combine and normalize the samples
+    merged_samples = numpy.hstack((sample1, sample2))
+    merged_samples = ((merged_samples.T)/(merged_samples.sum(1))).T
+    
+    cond_independence_sets = defaultdict(set)
+    
+    N = merged_samples.shape[1]
+    ones = numpy.ones((N,1), dtype=float)
+    for n1 in G.nodes():
+        n1_resp = merged_samples[n1,:]
+        n1_neighbors = set(G.neighbors(n1))
+        for n2 in n1_neighbors:
+            if n2 <= n1: continue
+            n2_resp = merged_samples[n2,:]
+            n2_neighbors = set(G.neighbors(n2))
+            neighbors = n1_neighbors.intersection(n2_neighbors) - set((n1, n2))
+            if len(neighbors) < order: continue
+            for covariates in combinations(neighbors, order):
+                predictors = numpy.hstack(
+                    (ones, merged_samples[numpy.array(covariates),:].T))
+                # test if node is independent of neighbors given for some subset
+                rv1, _, _, _ = lstsq(predictors, n1_resp)
+                rv2, _, _, _ = lstsq(predictors, n2_resp)
+                cor, pval =  pearsonr(n1_resp - rv1.dot(predictors.T), 
+                                      n2_resp - rv2.dot(predictors.T))
+                score = math.sqrt(N-order-3)*0.5*math.log((1+cor)/(1-cor))
+                if abs(score) < norm.isf(alpha/2): 
+                    print "Test: %s IND %s|{%s}" % (
+                        G.node[n1]['label'], G.node[n2]['label'], 
+                        ','.join(G.node[int(x)]['label'] for x in covariates)),
+                    print cor, pval, score, norm.isf(alpha/2)
+
+                    # we may have already removed this edge
+                    if G.has_edge(n1, n2): G.remove_edge(n1, n2)
+                    cond_independence_sets[(n1, n2)].update(
+                        int(x) for x in covariates)
+                    cond_independence_sets[(n2, n1)].update(
+                        int(x) for x in covariates)
+
+    return cond_independence_sets
+
+def estimate_pdag(sample1, sample2, labels):
+    skeleton = estimate_initial_skeleton(sample1, sample2, labels)
+    cond_independence_sets = defaultdict(set)
+    for i in xrange(1,6):
+        inner_cis = apply_pc_iteration( skeleton, sample1, sample2, i )
+        for key, vals in inner_cis.items():
+            cond_independence_sets[key].update(vals)
+        
+    #skeleton = break_cycles(skeleton)
+    est_G = skeleton.to_directed()
+    orient_v_structures(est_G, cond_independence_sets)
+    applied_rule = True
+    while apply_IC_rules(est_G): pass
+    #assert False
+    return est_G, cond_independence_sets
 
 def hierarchical_layout(real_G):
     level_grouped_nodes = defaultdict(list)
@@ -311,7 +461,7 @@ def hierarchical_layout(real_G):
         for i, node in enumerate(sorted(
                 nodes, key=lambda id: [int(x) for x in id.split("_")])):
             x_pos = float(i+1)/(len(nodes)+1)
-            pos[node] = numpy.array((x_pos, y_pos))
+            pos[node] = numpy.array((x_pos, y_pos+(x_pos/1.7)**2))
     
     return pos
 
@@ -360,6 +510,55 @@ def brute_force_find_all_consistent_dags(pdag, data):
     return dags
 
 
+def find_all_consistent_dags(pdag, cond_independence_sets, real_G):
+    initial_colliders = set(iter_colliders(pdag))
+    
+    def get_undirected_edges(G):
+        undirected_edges = []
+        for a, b in G.edges():
+            if G.has_edge(b, a):
+                undirected_edges.append( (a,b) )
+        return undirected_edges
+    
+    def orient_edge_and_propogate_changes(G, a, b):
+        G = G.copy()
+        G.remove_edge(b, a)
+        orient_v_structures(G, cond_independence_sets)
+        while apply_IC_rules(G): pass
+        # make sure that the resulting graph is acyclic
+        #if not nx.is_directed_acyclic_graph(G): return None
+        if any( collider not in initial_colliders 
+                for collider in  iter_colliders(G) ): 
+            return None
+        return G
+
+    #unoriented_v_structures = list(iter_unoriented_v_structures(pdag))
+    # the dags that we've found
+    dags = []
+    # stack containing both edges that we have already oriented, and 
+    # the current pdag after those edges have been oriented
+    stack = [ [[(a,b),], orient_edge_and_propogate_changes(pdag, a, b)]
+              for a, b in get_undirected_edges(pdag) ]
+    stack = [(x, g) for x, g in stack if g != None]
+    while len(stack) > 0:
+        oriented_edges, curr_pdag = stack.pop()
+        edges_to_orient = get_undirected_edges(curr_pdag)
+        # if there are no more edges to orient, then make sure that we haven't 
+        # already seen it and that it is acyclic, and add it
+        if len(edges_to_orient) == 0: 
+            if (not any(set(curr_pdag.edges()) == set(x.edges()) for x in dags)
+                    and nx.is_directed_acyclic_graph(curr_pdag)):
+                #plot_pdag(curr_pdag, real_G)
+                dags.append(curr_pdag)
+        else:
+            for a, b in edges_to_orient:
+                new_pdag = orient_edge_and_propogate_changes(curr_pdag, a, b)
+                if new_pdag != None:
+                    stack.append([oriented_edges + [(a, b),], new_pdag])
+    
+    return dags
+
+
 def plot_pdag(pdag, real_G):
     real_G_layout = hierarchical_layout(real_G)
     #nx.draw(est_G, nx.graphviz_layout(est_G,prog='twopi',args=''))
@@ -372,19 +571,18 @@ def plot_pdag(pdag, real_G):
 
 def main():
     real_G = simulate_causal_graph(2, 2)
-    #nx.draw(real_G, layout, node_size=1500, node_color='blue')
+    #nx.draw(real_G, hierarchical_layout(real_G), node_size=1500, node_color='blue')
     #plt.show()
     #return
 
-    labels, sample1 = simulate_data_from_causal_graph(real_G, 10, 0.5)
-    labels, sample2 = simulate_data_from_causal_graph(real_G, 10, 0.5)
-    
-    print "Penalized Regression:"
-    pdag = estimate_pdag(sample1, sample2, labels)
+    labels, sample1 = simulate_data_from_causal_graph(real_G, 3, 0.5)
+    labels, sample2 = simulate_data_from_causal_graph(real_G, 3, 0.5)
+    #print partial_corr(preprocessing.scale(numpy.hstack((sample1, sample2)).T).T)
+    #return
+    pdag, cond_independence_sets = estimate_pdag(sample1, sample2, labels)
     plot_pdag(pdag, real_G)
-    
-    for x in brute_force_find_all_consistent_dags(
-            pdag, numpy.hstack((sample1, sample2))):
+    for x in find_all_consistent_dags(
+            pdag, cond_independence_sets, real_G):
         print x.edges()
         plot_pdag(x, real_G)
     
